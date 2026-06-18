0a398c298c5fce6253ec9db986aef5f073837a7b
Shreya <shreyatripathi157@gmail.com>
2026-06-18 17:31:45 +0530

    initial project setup

---
+++ backend/index.js
@@ -0,0 +1,11 @@
+import express from 'express'
+import cors from 'cors'
+
+const app=express();
+
+app.use(cors());
+app.use(express.json());//parses incoming json requests and put data in req.body
+
+app.listen(5000,()=>{
+    console.log("Server running on port 5000");
+});
