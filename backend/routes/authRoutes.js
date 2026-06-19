e873ddad84dd43b1714cbbc7f50ae15798dd88ed
Shreya <shreyatripathi157@gmail.com>
2026-06-19 17:41:18 +0530

    implement jwt based user login

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import pool from "../db.js";
 import bcrypt from "bcrypt";
+import jwt from "JsonWebToken";
 
 const router=Router();
 
@@ -30,9 +31,54 @@ router.post("/register",async(req,res)=>{
     });
 }
 catch(err){
-    console.log(err);
+    console.error(err);
     res.status(500).send("server error");
 }
+});
+
+router.post("/login",async(req,res)=>{
+    try{
+        const {email,password}=req.body;
+        if(!email || !password){
+            return res.status(400).json({
+                message:"email and password is required"
+            });
+        }
+        const result=await pool.query(
+            "SELECT * FROM users WHERE email=$1",[email]
+        );
+        if(result.rows.length==0){//email does not exist
+            return res.status(400).json({
+                message:"invalid email or password"
+            });
+        }
+         const user=result.rows[0];
+        const isMatch=await bcrypt.compare(password,user.password);
+        if(!isMatch){
+            return res.status(400).json({
+                message:"invalid email or password"
+            });
+        }
+        
+            const token=jwt.sign(//generating jwt token if the passsword matches
+                {
+                    id:user.id
+                },process.env.JWT_SECRET,
+                {
+                    expiresIn:"7d"
+                }
+            );
+            return res.status(200).json({
+                message:"Login successful!",
+                token
+            });
+        
+        
+    }
+    catch(err){
+        console.error(err);
+        return res.status(500).json({message:"internal server error"});
+    }
 });
 
 
